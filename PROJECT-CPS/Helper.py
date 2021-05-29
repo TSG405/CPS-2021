0b9d75162843e3018ef6b984994b2f52f951dfed
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2021-05-29 18:05:45 +0530

    Create Helper.py

---
+++ PROJECT-CPS/Helper.py
@@ -0,0 +1,9 @@
+# Cleaning and re-initializing DatabaseList!
+
+DatabaseList = open('SLOTLIST.txt',"w")
+for datas in range(50): DatabaseList.write("null ")
+DatabaseList.close()
+
+'''
+@ ~ TSG405,2021
+'''

9bbeace59ca03ad9743ad41c88d8dce15e74a7ff
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2021-05-29 18:10:13 +0530

    Create GeneratingSlots.py

---
+++ PROJECT-CPS/GeneratingSlots.py
@@ -0,0 +1,31 @@
+def GivingSlots(SlotList,NumberOfVacantSlots):
+    
+    fh = open('QRcode_result.txt')                                                                 # Accessing QR-Response Text!
+    for i in fh:
+        l = i.split('-')
+    carNumber = l[-1]
+    fh.close()
+
+    # Slot Registration ------------------------------------------------------------------------------------------------------------------------------------------------------------
+    if NumberOfVacantSlots >= 1:
+        for j in range(len(SlotList)):
+
+            if SlotList[j] == 'null':
+                SlotList[j] = carNumber
+                print("\n\n-------------------------------------------------------")
+                print('Your Slot Number is ---', j+1)
+                print("-------------------------------------------------------\n\n")
+                break
+    else:
+        print('SORRY! Currently No Slot is Available!!!')
+    
+    return (SlotList)
+
+# Driver Code ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    GivingSlots()
+    
+ 
+'''
+@ ~ TSG405,2021
+'''

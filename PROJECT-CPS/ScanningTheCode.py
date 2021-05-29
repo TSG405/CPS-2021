cd6fd0d0a1bdd0b0f83dff7354543e55d2e3e813
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2021-05-29 18:12:24 +0530

    Create ScanningTheCode.py

---
+++ PROJECT-CPS/ScanningTheCode.py
@@ -0,0 +1,51 @@
+from pyzbar import pyzbar
+import cv2
+
+
+def read_qrcode(frame):
+    qrcodes = pyzbar.decode(frame)
+    
+    for code in qrcodes:
+        x, y, w, h = code.rect
+
+        # Creating Framework
+        qrcode_info = code.data.decode('utf-8')
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Display Scanned QR Text (Immediate Response!)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, qrcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
+
+        # Converting and saving the response in a text file -- 'QRcode_result.txt'
+        with open("QRcode_result.txt", mode='w') as file:
+            file.write("Recognized QRode:" + qrcode_info)
+            
+        print(qrcode_info)
+            
+    return frame
+
+# Driver Code -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def main():
+
+    camera = cv2.VideoCapture(0)
+    ret, frame = camera.read()
+
+    while ret:
+        ret, frame = camera.read()
+        frame = read_qrcode(frame)
+        cv2.imshow('QR code reader', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+'''
+@ ~ TSG405,2021
+'''
